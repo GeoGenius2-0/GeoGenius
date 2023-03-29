863fb868906ba3055e7ef1f80555e7f0af90d608
jahshiekwade58@gmail.com <jahshiek@DESKTOP-3EP0RRM.localdomain>
2023-03-29 14:21:50 -0400

    work in progress2

--- code/air.js
+++ code/air.js
@@ -1,3 +1,4 @@
+const locationInp = document.querySelector("#location")
 const latInp = document.querySelector("#latitude")
 const longInp = document.querySelector("#longitude")
 const airQuality = document.querySelector(".air-quality")
@@ -6,54 +7,81 @@ const srchBtn = document.querySelector(".search-btn")
 const componentsEle = document.querySelectorAll(".component-val")
 
 const appId = "f42bda7ce438693d1b9966abceb83f10" 
+const apiKey = '61380ea7a1e94e3a834965ee9dfac99f'
 
 const getUserLocation = () => {
-    const lat = latInp.value;
-    const long = longInp.value;
-  
-    if (lat && long) {
+    const location = locationInp.value;
+
+    if (location) {
+      // Get coordinates from OpenCage Geocoding API using user input location
+      getCoordinates(location);
+    } else if (latInp.value && longInp.value) {
       // Get Air data from weather API using user input coordinates
-      getAirQuality(lat, long);
+      getAirQuality(latInp.value, longInp.value);
     } else if (navigator.geolocation) {
       // Get user Location using geolocation API
       navigator.geolocation.getCurrentPosition(onPositionGathered, onPositionGatherError);
     } else {
-      onPositionGatherError({ message: "Can't Access your location. Please enter your co-ordinates" });
+      onPositionGatherError({ message: "Can't Access your location. Please enter your location or co-ordinates" });
     }
-  };
-  
+};
 
 const onPositionGathered = (pos) => {
-	let lat = pos.coords.latitude, long = pos.coords.longitude// an add tFixed to shorten
+  let lat = pos.coords.latitude, long = pos.coords.longitude
 
-	// Set values of for user to know
-	latInp.value = lat
-	longInp.value = long
+  // Set values of for user to know
+  latInp.value = lat
+  longInp.value = long
 
-	// Get Air data from weather API
-	getAirQuality(lat, long)
+  // Get location from OpenCage Geocoding API
+  getLocation(lat, long);
+
+  // Get Air data from weather API
+  getAirQuality(lat, long)
+}
+
+const getCoordinates = async (location) => {
+  const url = `https://api.opencagedata.com/geocode/v1/json?q=${location}&key=${apiKey}`;
+  const rawData = await fetch(url).catch(err => {})
+  const data = await rawData.json()
+  console.log(data)
+  const { lat, lng } = data.results[0].geometry;
+  locationInp.innerText = data.results[0].formatted;
+  latInp.value = lat;
+  longInp.value = lng;
+  getAirQuality(lat, lng);
+  getLocation(lat, lng);
 }
 
 const getAirQuality = async (lat, long) => {
-	// Get data from api
-	const rawData = await fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${long}&appid=${appId}`).catch(err => {
-	})
-	const airData = await rawData.json()
-	setValuesOfAir(airData)
-	setComponentsOfAir(airData)
+  // Get data from api
+  const rawData = await fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${long}&appid=${appId}`).catch(err => {})
+  const airData = await rawData.json()
+  setValuesOfAir(airData)
+  setComponentsOfAir(airData)
+}
+
+const getLocation = async (lat, long) => {
+  const apiKey = '61380ea7a1e94e3a834965ee9dfac99f';
+  const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=${apiKey}`;
+  const rawData = await fetch(url).catch(err => {})
+  const data = await rawData.json()
+  const location = data.results[0].formatted;
+  locationInp.value = location;
 }
 
 const setValuesOfAir = airData => {
-	const aqi = airData.list[0].main.aqi
-	let airStat = "", color = ""
+  const aqi = airData.list[0].main.aqi
+  let airStat = "", color = ""
 
-	// Set Air Quality Index
-	airQuality.innerText = aqi
+  // Set Air Quality Index
+  airQuality.innerText = aqi
 
-	// Set status of air quality
-	if (aqi === 1) {
-		airStat = "Good"
-		color = "rgb(19, 201, 28)"
+  // Set status of air quality
+  if (aqi === 1) {
+    airStat = "Good"
+    color = "rgb(19, 201, 28)"
+
 	} else if (aqi === 2) {
 		airStat = "Fair"
 		color = "rgb(15, 134, 25)"
@@ -90,3 +118,4 @@ srchBtn.addEventListener("click", () => {
     getUserLocation()
 })
 
+  
--- code/index.js
+++ code/index.js
@@ -1,51 +1,12 @@
-const airQualityKey = api.Air_Quality_API_KEY
+const airQualityKey = 'f42bda7ce438693d1b9966abceb83f10'
 
-
-
-// let typing = document.querySelector("#typing")
-//   const text = "This is a typing effect";
-//   let index = 0;
-//  let interval =  setInterval(() => {
-//     typing.innerText += text[index];
-//     index++;
-//     if (index === text.length) {
-//       clearInterval(interval);
-//     }
-//   }, 100);
-// next get an array 
 const sentences = [
-    "This is MF.Doom",
+    "Unit-6 API Project",
     "Hello, world!",
-    "Lorem ipsum dolor sit amet",
-    "Nayan is a cool guy",
-    'Thank you ben'
+    "Clean air FTW",
+    "#BreatheQualityAir",
   ];
-  
-  //Mine
-  
-  //   let intervalId;
-  //   const typing = document.querySelector("#typing");
-  
-  //   function startTyping() {
-  //     let sentenceIndex = Math.floor(Math.random() * sentences.length);
-  //     let sentence = sentences[sentenceIndex];
-  //     let index = 0;
-  //     intervalId = setInterval(() => {
-  //       typing.innerHTML += sentence[index];
-  //       index++;
-  //       if (index === sentence.length) {
-  //         clearInterval(intervalId);
-  //         setTimeout(() => {
-  //           typing.innerText = "";
-  //           startTyping();
-  //         }, 2000);
-  //       }
-  //     }, 100);
-  //   }
-  
-  // startTyping()
-  
-  //chat gpt 
+
   let intervalId;
   const typing = document.querySelector("#typing");
   
@@ -109,35 +70,27 @@ const sentences = [
           console.log('My General Position:', position);
           const long = position.coords.longitude;
           const lat = position.coords.latitude;
-          const response = await fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${long}&lon=${lat}&appid=f42bda7ce438693d1b9966abceb83f10`);
+          localStorage.setItem('longitude', long);
+          localStorage.setItem('latitude', lat);
+          const response = await fetch(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${long}&lon=${lat}&appid=${airQualityKey}`);
           const data = await response.json();
           console.log(data)
           // console.log(data.list['0'].components)
-      
         })
       }
-  
     }
     catch (error) {
       console.log(error)
     }
   }
+
+//   const longitude = localStorage.getItem('longitude');
+// const latitude = localStorage.getItem('latitude');
+
   
   
   
   
   
-  // const lat = 40.712776;
-  // const lon = -74.005974;
-  // const apiKey = 'YOUR_API_KEY'; // Replace with your API key
   
-  // const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=${apiKey}`;
-  
-  // fetch(url)
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     const location = data.results[0].formatted;
-  //     console.log(location); // "New York, NY, USA"
-  //   })
-  //   .catch(error => console.error(error));
-  
+ 
